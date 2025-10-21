64ea43298477d0744eeffcb5ed50086c00f71185
sejung2 <seingdev@gmail.com>
2025-10-21 13:45:12 +0900

    init: 초기설정 완료

---
+++ ReVeauty/src/main/java/com/project/reveauty/ReVeautyApplication.java
@@ -0,0 +1,13 @@
+package com.project.reveauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReVeautyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReVeautyApplication.class, args);
+    }
+
+}
