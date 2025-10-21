c2e5c14b8069748f3a75f975e7eb8a57dd871190
sejung2 <seingdev@gmail.com>
2025-10-21 17:54:24 +0900

    rename: 프로젝트명 변경
    서비스 주제 변경으로 인한 이름 변경

--- Shift/src/main/java/com/project/reveauty/ServletInitializer.java
+++ Shift/src/main/java/com/project/reveauty/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ReVeautyApplication.class);
+        return application.sources(ShiftApplication.class);
     }
 
 }
--- Shift/src/main/java/com/project/reveauty/ShiftApplication.java
+++ Shift/src/main/java/com/project/reveauty/ShiftApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReVeautyApplication {
+public class ShiftApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReVeautyApplication.class, args);
+        SpringApplication.run(ShiftApplication.class, args);
     }
 
 }
