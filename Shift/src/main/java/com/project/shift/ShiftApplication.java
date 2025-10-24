355f5629b24b62599e53f43c2f52eb9566af206a
hyogyeong <cdkrcd8@gmail.com>
2025-10-24 18:13:36 +0900

    add : component scan 추가

--- Shift/src/main/java/com/project/shift/ShiftApplication.java
+++ Shift/src/main/java/com/project/shift/ShiftApplication.java
@@ -2,8 +2,10 @@ package com.project.shift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.project.shift"})
 public class ShiftApplication {
 
     public static void main(String[] args) {
