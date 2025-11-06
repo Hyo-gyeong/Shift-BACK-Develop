07ea9f2d7f32264b73b660a2389da3c058ce5dde
James Yeo <ymg090724@naver.com>
2025-11-06 11:47:29 +0900

    PROD-003 카테고리목록조회 기능추가

--- Shift/src/main/java/com/project/shift/product/dao/CategoryDAO.java
+++ Shift/src/main/java/com/project/shift/product/dao/CategoryDAO.java
@@ -1,5 +1,25 @@
 package com.project.shift.product.dao;
 
-public class CategoryDAO {
+import com.project.shift.product.dto.CategoryDTO;
+import com.project.shift.product.entity.Category;
+import com.project.shift.product.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class CategoryDAO implements ICategoryDAO {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public List<CategoryDTO> getCategoryList() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream()
+                .map(CategoryDTO::new)
+                .collect(Collectors.toList());
+    }
 }
--- Shift/src/main/java/com/project/shift/product/entity/Category.java
+++ Shift/src/main/java/com/project/shift/product/entity/Category.java
@@ -1,5 +1,32 @@
 package com.project.shift.product.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "CATEGORIES")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CATEGORY_ID")
+    private Long categoryId;
+
+    @Column(name = "CATEGORY_NAME", nullable = false, length = 50)
+    private String categoryName;
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
