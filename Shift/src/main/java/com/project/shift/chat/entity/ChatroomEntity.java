35a4ac9bf0dbf44941804687e4835a077bd4f3a7
hyogyeong <cdkrcd8@gmail.com>
2025-11-07 16:20:48 +0900

    add : ChatroomEntity 생성

--- Shift/src/main/java/com/project/shift/chat/entity/ChatroomEntity.java
+++ Shift/src/main/java/com/project/shift/chat/entity/ChatroomEntity.java
@@ -1,35 +1,49 @@
-package com.project.shift.chat.dto;
+package com.project.shift.chat.entity;
 
 import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
+
+import com.project.shift.chat.dto.ChatroomDTO;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "Chatrooms")
+@Table(name="Chatrooms")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Chatroom {
+@Builder
+public class ChatroomEntity {
 
     @Id
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "seq_chatrooms"
+    )
+    @SequenceGenerator(
+        name = "seq_chatrooms",
+        sequenceName = "seq_chatrooms",
+        allocationSize = 1
+    )
     @Column(name = "CHATROOM_ID")
-    private int chatroomId;
+    private long chatroomId;
 
     @Column(name = "FROM_USER_ID")
-    private int fromUserId;
+    private long fromUserId;
 
     @Column(name = "TO_USER_ID")
-    private int toUserId;
+    private long toUserId;
 
     @Column(name = "CHATROOM_NAME")
     private String chatroomName;
@@ -43,4 +57,18 @@ public class Chatroom {
 
     @Column(name = "IS_DARK_MODE", length = 1)
     private String isDarkMode; // DEFAULT 'N', 'Y' 또는 'N'
+    
+    // DTO -> Entity 변환
+    public ChatroomEntity toEntity(ChatroomDTO dto) {
+        return ChatroomEntity.builder()
+                .chatroomId(dto.getChatroomId())
+                .fromUserId(dto.getFromUserId())
+                .toUserId(dto.getToUserId())
+                .chatroomName(dto.getChatroomName())
+                .connectionStatus(dto.getConnectionStatus())
+                .connectionTime(dto.getConnectionTime())
+                .isDarkMode(dto.getIsDarkMode())
+                .build();
+    }
+
 }
