90db76c2749b71c4a4a25df655b04791a8a93674
hyogyeong <cdkrcd8@gmail.com>
2025-11-07 16:20:05 +0900

    add : UserEntity 생성

--- Shift/src/main/java/com/project/shift/chat/entity/UserEntity.java
+++ Shift/src/main/java/com/project/shift/chat/entity/UserEntity.java
@@ -1,20 +1,39 @@
-package com.project.shift.chat.dto;
+package com.project.shift.chat.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.project.shift.chat.dto.UserDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "Users")
+@Table(name="Users")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+@Builder
+public class UserEntity {
+	
     @Id
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "seq_users"
+    )
+    @SequenceGenerator(
+        name = "seq_users",
+        sequenceName = "seq_users",
+        allocationSize = 1
+    )
     @Column(name = "user_id")
-    private Integer userId;
+    private long userId;
 
     @Column(name = "login_id", unique = true)
     private String loginId;
@@ -32,11 +51,27 @@ public class User {
     private String address;
 
     @Column(name = "points")
-    private Integer points; // DEFAULT 0
+    private int points; // DEFAULT 0
 
     @Column(name = "refresh_token")
     private String refreshToken;
 
     @Column(name = "admin_flag", length = 1)
     private String adminFlag; // DEFAULT 'N', 'Y' 또는 'N'
+    
+    // DTO -> Entity 변환
+    public static UserEntity toEntity(UserDTO dto) {
+        return UserEntity.builder()
+                .userId(dto.getUserId())
+                .loginId(dto.getLoginId())
+                .password(dto.getPassword())
+                .name(dto.getName())
+                .phone(dto.getPhone())
+                .address(dto.getAddress())
+                .points(dto.getPoints())
+                .refreshToken(dto.getRefreshToken())
+                .adminFlag(dto.getAdminFlag())
+                .build();
+    }
+    
 }
