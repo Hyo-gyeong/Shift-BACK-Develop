d5d06aacbfce3145edc26f694dbaa6594a252acc
hyogyeong <cdkrcd8@gmail.com>
2025-11-08 18:07:48 +0900

    fix : Entity 이름 변경으로 이한 오류 수정 테스트DB에서 사용하던 필드명을 개발DB에서 사용하는 이름으로 변경

--- Shift/src/main/java/com/project/shift/chat/repository/ChatroomRepository.java
+++ Shift/src/main/java/com/project/shift/chat/repository/ChatroomRepository.java
@@ -9,12 +9,12 @@ import com.project.shift.chat.entity.ChatroomEntity;
 
 public interface ChatroomRepository extends JpaRepository<ChatroomEntity, Long>{
 
-	List<ChatroomEntity> findByFromId(int userId);
+	List<ChatroomEntity> findByFromUserId(int userId);
 	
 	// 두 사용자가 서로 대화하는 방의 ID 조회(사용자별로 chatroomId가 다름)
-    @Query("SELECT c.id FROM Chatroom c " +
-           "WHERE (c.fromId = :fromId AND c.toId = :toId) " +
-           "   OR (c.fromId = :toId AND c.toId = :fromId)")
+    @Query("SELECT c.id FROM ChatroomEntity c " +
+           "WHERE (c.fromUserId = :fromId AND c.toUserId = :toId) " +
+           "		OR (c.fromUserId = :toId AND c.toUserId = :fromId)")
     List<Integer> findChatroomIdsForUsers(int fromId, int toId);
     
 }
