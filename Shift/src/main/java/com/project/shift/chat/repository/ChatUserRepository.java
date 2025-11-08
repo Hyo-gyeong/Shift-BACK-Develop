454c8630b6d243186e08a56677c5cf6ada297a6c
hyogyeong <cdkrcd8@gmail.com>
2025-11-08 18:06:42 +0900

    fix : Entity 이름 변경으로 인한 오류 수정

--- Shift/src/main/java/com/project/shift/chat/repository/ChatUserRepository.java
+++ Shift/src/main/java/com/project/shift/chat/repository/ChatUserRepository.java
@@ -1,9 +1,12 @@
 package com.project.shift.chat.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.shift.chat.entity.ChatUserEntity;
+import com.project.shift.user.entity.UserEntity;
 
 public interface ChatUserRepository extends JpaRepository<ChatUserEntity, Long>{
-	
+	ChatUserEntity findByUserId(int userId);
 }
