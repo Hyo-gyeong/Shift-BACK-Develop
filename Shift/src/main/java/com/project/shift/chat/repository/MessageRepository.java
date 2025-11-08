89f95805b53feccdf6f26479b0ae692d1efe07b4
hyogyeong <cdkrcd8@gmail.com>
2025-11-08 17:44:06 +0900

    fix : entity이름 변경으로 인한 오류 수정 쿼리문의 Message를 MessageEntity로 변경 param 추가

--- Shift/src/main/java/com/project/shift/chat/repository/MessageRepository.java
+++ Shift/src/main/java/com/project/shift/chat/repository/MessageRepository.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.project.shift.chat.entity.MessageEntity;
 
 public interface MessageRepository extends JpaRepository<MessageEntity, Long>{
 		
 	// 두 참여자 같이 참여한 채팅방의 메시지 내역 반환
-	@Query("SELECT m FROM Message m " +
+	@Query("SELECT m FROM MessageEntity m " +
             "WHERE m.chatRoomId IN :chatroomIds " +
             "ORDER BY m.id ASC")
-     List<MessageEntity> findMessagesByChatroomIds(List<Integer> chatroomIds);
+     List<MessageEntity> findMessagesByChatroomIds(@Param("chatroomIds") List<Integer> chatroomIds);
 
 }
