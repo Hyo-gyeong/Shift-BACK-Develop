2d380959e2385351dea7a1669adf18c07b2b9666
hyogyeong <cdkrcd8@gmail.com>
2025-11-09 16:30:14 +0900

    add : FriendService 추가

---
+++ Shift/src/main/java/com/project/shift/chat/service/FriendService.java
@@ -0,0 +1,32 @@
+package com.project.shift.chat.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.shift.chat.dao.FriendDAO;
+import com.project.shift.chat.dto.FriendDTO;
+import com.project.shift.chat.entity.FriendEntity;
+
+@Service
+public class FriendService {
+
+	@Autowired
+	FriendDAO dao;
+	
+	public List<FriendDTO> getUserFriends(int userId){
+		List<FriendEntity> entityList = dao.getUserFriends(userId);
+		List<FriendDTO> dtoList = new ArrayList<FriendDTO>();
+		for (FriendEntity e : entityList) {
+			dtoList.add(FriendDTO.toDto(e));
+		}
+		return dtoList;
+	}
+	
+	public void addFriend(int userId, int friendId) {
+		dao.insertFriend(userId, friendId);
+	}
+
+}
