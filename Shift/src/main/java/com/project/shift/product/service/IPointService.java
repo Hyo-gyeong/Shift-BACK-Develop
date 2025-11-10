c5138591bd5cd0ec343218849d21224393ee6f56
Suhyun03Lim <suoil88@naver.com>
2025-11-10 17:26:38 +0900

    add : PROD-010 금액권 정보 조회 기능
    - 금액권은 상품 카테고리 중 CATEGORY_ID = 3 으로 구분
    - 조회 정보에는 금액권 대표 이미지(1개), 설명, 최소/최대 금액 범위가 포함
    - 금액권의 실 주문 생성은 SHOP-016 (금액권 주문 생성 API)에서 처리

--- Shift/src/main/java/com/project/shift/product/service/IPointService.java
+++ Shift/src/main/java/com/project/shift/product/service/IPointService.java
@@ -1,5 +1,10 @@
 package com.project.shift.product.service;
 
-public interface IPointService {
+import com.project.shift.product.dto.PointDTO;
 
+/**
+ * 금액권 페이지 정보 조회 서비스 인터페이스
+ */
+public interface IPointService {
+    PointDTO getPointPageInfo();
 }
