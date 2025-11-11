0107148a64396e928f6db78a769575d42a2cc923
hyogyeong <cdkrcd8@gmail.com>
2025-11-11 19:53:22 +0900

    add : ChatroomDAO 채팅방 저장, 삭제 함수 생성

--- Shift/src/main/java/com/project/shift/chat/dao/ChatroomDAO.java
+++ Shift/src/main/java/com/project/shift/chat/dao/ChatroomDAO.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.project.shift.chat.dto.ChatroomDTO;
 import com.project.shift.chat.entity.ChatroomEntity;
 import com.project.shift.chat.repository.ChatroomRepository;
+
 
 @Component
 public class ChatroomDAO {
@@ -24,7 +24,20 @@ public class ChatroomDAO {
 	}
 	
 	public ChatroomEntity insertChatroom(ChatroomEntity entity) {
-		return chatroomRepo.save(entity);
+	    // 이미 존재하는 채팅방은 저장(덮어쓰기)불가
+	    if (chatroomRepo.existsById(entity.getChatroomId())) {
+	        throw new IllegalStateException("Chatroom already exists with ID: " + entity.getChatroomId());
+	    }
+	    // 채팅방 생성
+	    return chatroomRepo.save(entity);
+	}
+	
+	public boolean deleteById(long chatroomId) {
+		if (chatroomRepo.existsById(chatroomId)) {
+	        chatroomRepo.deleteById(chatroomId);
+	        return true;
+	    }
+	    return false;
 	}
 	
 }
