b1c96a07c306397bdfb3b59570274aa7fdb49b5b
sejung2 <seingdev@gmail.com>
2025-11-16 20:40:37 +0900

    add: logout 기능 구현
    - 로그아웃 성공 시 기존 리프레시 토큰 삭제

--- Shift/src/main/java/com/project/shift/auth/repository/AuthRepository.java
+++ Shift/src/main/java/com/project/shift/auth/repository/AuthRepository.java
@@ -2,6 +2,8 @@ package com.project.shift.auth.repository;
 
 import com.project.shift.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,4 +15,9 @@ public interface AuthRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUserId(Long userId);
 
     Optional<UserEntity> findByRefreshToken(String refreshToken);
+
+    @Modifying
+    @Query(value = "UPDATE UserEntity u SET u.refreshToken = NULL WHERE u.userId = :userId")
+    void updateRefreshTokenById(Long userId);
+
 }
