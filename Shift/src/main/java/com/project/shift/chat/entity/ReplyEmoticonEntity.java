d695b9db80f47d86a8524ff2f50c67d6b7c24a27
hyogyeong <cdkrcd8@gmail.com>
2025-11-17 17:42:28 +0900

    add : ReplyEmoticionEntity 추가 메시지별 이모티콘 반응을 위해 추가, 다대다 채팅에서도 사용 가능하도록 userId추가

---
+++ Shift/src/main/java/com/project/shift/chat/entity/ReplyEmoticonEntity.java
@@ -0,0 +1,62 @@
+package com.project.shift.chat.entity;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.project.shift.chat.dto.ChatroomDTO;
+import com.project.shift.chat.dto.ReplyEmoticonDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="REPLY_EMOTICONS")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReplyEmoticonEntity {
+
+	@Id
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "SEQ_REPLY_EMOTICONS"
+    )
+    @SequenceGenerator(
+        name = "SEQ_REPLY_EMOTICONS",
+        sequenceName = "SEQ_REPLY_EMOTICONS",
+        allocationSize = 1
+    )
+    @Column(name = "REPLY_EMOTICON_ID")
+    private long replyEmoticonId;
+
+    @Column(name = "MESSAGE_ID")
+    private long messageId;
+
+    @Column(name = "USER_ID")
+    private long userId;
+    
+    @Column(name = "TYPE", length = 1, columnDefinition = "CHAR(3)")
+    private String type;
+    
+    // DTO -> Entity 변환
+    public static ReplyEmoticonEntity toEntity(ReplyEmoticonDTO dto) {
+        return ReplyEmoticonEntity.builder()
+                .replyEmoticonId(dto.getReplyEmoticonId())
+                .messageId(dto.getMessageId())
+                .userId(dto.getUserId())
+                .type(dto.getType())
+                .build();
+    }
+
+}
