1b23ff6f46c78a27a7f835b56b632373c7bb5dcd
hyogyeong <cdkrcd8@gmail.com>
2025-11-17 17:06:43 +0900

    refactor : 변경된 ERD에 맞게 MessageDTO 전면 수정 필드 추가 및 삭제, 필드명 변경

--- Shift/src/main/java/com/project/shift/chat/dto/MessageDTO.java
+++ Shift/src/main/java/com/project/shift/chat/dto/MessageDTO.java
@@ -24,32 +24,25 @@ public class MessageDTO {
     }
 	@Transient //DB와 매핑하지 않는 필드
 	private MessageType type;
-	@Transient
-    private int toId;
-	@Transient
-	private int fromId; // 로그인 구현 후 없애야 함
 	
     private long messageId;
     private long chatroomId;
-    private String isFromUser;
-    private String isRead; // DEFAULT 'N', 'Y' 또는 'N'
-    private Date sentDate;
-    @Column(name="MESSAGE_CONTENT")
+    private long userId;
+    private Date sendDate;
     private String content;
-    private String isGift; // DEFAULT 'N', 'Y' 또는 'N'
-    private String isEmoji; // DEFAULT 'N', 'Y' 또는 'N'
+    private String isGift;
+    private int unreadCount;
 
     // Entity -> DTO 변환
     public static MessageDTO toDto(MessageEntity entity) {
         return MessageDTO.builder()
                 .messageId(entity.getMessageId())
                 .chatroomId(entity.getChatroomId())
-                .isFromUser(entity.getIsFromUser())
-                .isRead(entity.getIsRead())
-                .sentDate(entity.getSentDate())
+                .userId(entity.getUserId())
+                .sendDate(entity.getSendDate())
                 .content(entity.getContent())
                 .isGift(entity.getIsGift())
-                .isEmoji(entity.getIsEmoji())
+                .unreadCount(entity.getUnreadCount())
                 .build();
     }
 
