c7dd0a1d057974457738328ea50b9a3309ae9f6e
hyogyeong <cdkrcd8@gmail.com>
2025-11-17 14:04:38 +0900

    rename : ChatController를 MessageController로 수정 refactor : requestmapping 진입점을 messages로 통일

--- Shift/src/main/java/com/project/shift/chat/controller/MessageController.java
+++ Shift/src/main/java/com/project/shift/chat/controller/MessageController.java
@@ -14,6 +14,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.shift.chat.dto.MessageDTO;
@@ -26,7 +27,8 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-public class ChatController {
+@RequestMapping("/messages")
+public class MessageController {
 	private final MessageService messageService;
 	private final ChatroomService chatroomService;
 	private final SimpMessagingTemplate messagingTemplate;
@@ -92,7 +94,7 @@ public class ChatController {
     }
 	
 	// 채팅기록 불러오기
-	@PostMapping("/chatroom/chat-history")
+	@PostMapping("/history")
 	public List<MessageDTO> getChatHistory(@RequestBody Map<String, Object> payload) {
 		int fromId = Integer.parseInt(payload.get("fromId").toString());
 	    int toId = Integer.parseInt(payload.get("toId").toString());
