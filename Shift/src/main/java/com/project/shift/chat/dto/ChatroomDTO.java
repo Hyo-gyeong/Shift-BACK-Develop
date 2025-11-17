06a7f0289d0e54d63f62b661d60263da960b347d
hyogyeong <cdkrcd8@gmail.com>
2025-11-17 19:42:47 +0900

    refactor : ChatroomDTO 전면 수정

--- Shift/src/main/java/com/project/shift/chat/dto/ChatroomDTO.java
+++ Shift/src/main/java/com/project/shift/chat/dto/ChatroomDTO.java
@@ -10,7 +10,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
@@ -19,30 +18,16 @@ import lombok.Setter;
 public class ChatroomDTO {
 
     private long chatroomId;
-    @Setter
-    private long fromUserId;
-    @Setter
-    private long toUserId;
-    @Setter
-    private String chatroomName;
-    @Setter
-    private String connectionStatus; // 'ON' 또는 'OF'
-    @Setter
+    private String lastMsgContent;
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date connectionTime;
-    @Setter
-    private String isDarkMode; // 'Y' 또는 'N'
+    private Date lastMsgDate;
 
     // Entity -> DTO 변환
     public static ChatroomDTO toDto(ChatroomEntity entity) {
         return ChatroomDTO.builder()
                 .chatroomId(entity.getChatroomId())
-                .fromUserId(entity.getFromUserId())
-                .toUserId(entity.getToUserId())
-                .chatroomName(entity.getChatroomName())
-                .connectionStatus(entity.getConnectionStatus())
-                .connectionTime(entity.getConnectionTime())
-                .isDarkMode(entity.getIsDarkMode())
+                .lastMsgContent(entity.getLastMsgContent())
+                .lastMsgDate(entity.getLastMsgDate())
                 .build();
     }
     
