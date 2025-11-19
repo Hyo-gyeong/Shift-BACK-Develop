81d482c472f2471445effd227f8e2dc20ea997d3
hyogyeong <cdkrcd8@gmail.com>
2025-11-19 14:49:22 +0900

    add : 전송된 채팅 메시지값 + 채팅방 접속시간 합친 dto생성

---
+++ Shift/src/main/java/com/project/shift/chat/dto/MessageUserDTO.java
@@ -0,0 +1,16 @@
+package com.project.shift.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MessageUserDTO {
+
+	private MessageDTO messageDTO;
+	private ChatroomUserDTO chatroomUserDTO;
+}
