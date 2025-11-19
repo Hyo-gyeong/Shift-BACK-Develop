46aface755afff96f52b1c3fc88f62e00272d44a
hyogyeong <cdkrcd8@gmail.com>
2025-11-19 14:21:50 +0900

    refactor : ChatUserDAO에서 불필요한 함수 삭제

--- Shift/src/main/java/com/project/shift/chat/dao/ChatUserDAO.java
+++ Shift/src/main/java/com/project/shift/chat/dao/ChatUserDAO.java
@@ -1,13 +1,9 @@
 package com.project.shift.chat.dao;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.project.shift.chat.entity.ChatUserEntity;
 import com.project.shift.chat.repository.ChatUserRepository;
-import com.project.shift.chat.repository.ChatroomRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,14 +12,6 @@ import lombok.RequiredArgsConstructor;
 public class ChatUserDAO {
 
 	private final ChatUserRepository chatUserRepo;
-
-	public ChatUserEntity getUserInfo(int id) {
-		return chatUserRepo.findByUserId(id);
-	}
-	
-	public List<ChatUserEntity> getUserInfoByIds(List<Integer> userIds) {
-		return chatUserRepo.findUserInfoByIds(userIds);
-	}
 
 	public ChatUserEntity getUserInfoByPhone(String phone) {
 		return chatUserRepo.findByPhoneFlexible(phone);
