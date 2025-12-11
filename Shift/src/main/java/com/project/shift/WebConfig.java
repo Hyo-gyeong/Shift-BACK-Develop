7dcc24ebbdd3482d84a0b25748306eb7aa895a1d
hyogyeong <cdkrcd8@gmail.com>
2025-12-11 17:12:52 +0900

    add : cors origin envirenment variable

--- Shift/src/main/java/com/project/shift/WebConfig.java
+++ Shift/src/main/java/com/project/shift/WebConfig.java
@@ -1,16 +1,20 @@
 package com.project.shift;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+	@Value("${cors.allowed-origins}")
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 URL 허용
-                .allowedOrigins("http://localhost:3000") // React 주소
+                .allowedOrigins(allowedOrigins) // React 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
