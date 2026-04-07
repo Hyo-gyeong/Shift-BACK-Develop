e51036b978dfd4c6cd298612e0506d496e1cb7b6
hyogyeong <cdkrcd8@gmail.com>
2026-04-07 18:46:42 +0900

    chore : import문, 어노테이션 정리

--- Shift-BACK-Develop/src/main/java/com/project/shift/user/entity/UserEntity.java
+++ Shift-BACK-Develop/src/main/java/com/project/shift/user/entity/UserEntity.java
@@ -1,19 +1,27 @@
 package com.project.shift.user.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import java.sql.Timestamp;
+
 import org.hibernate.annotations.SQLRestriction;
 
-import java.sql.Timestamp;
+// 가독성이 떨어지고 클래스 충돌 가능성이 있고 유지보수가 어렵기 때문에 하나 씩 import
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "USERS")
-@SequenceGenerator(
-        name = "SEQ_USERS_GENERATOR",
-        sequenceName = "seq_users",
-        allocationSize = 1
-)
-@SQLRestriction("DELETED_AT IS NULL") //DELETED_AT이 NULL인 값만 조회하도록 설정
+@SQLRestriction("DELETED_AT IS NULL") //모든 SQL 쿼리에 공통된 WHERE 조건을 추가, DELETED_AT이 NULL인 값만 조회
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -22,11 +30,19 @@ import java.sql.Timestamp;
 public class UserEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_USERS_GENERATOR")
-    @Column(name = "USER_ID", nullable = false, updatable = false)
+    @GeneratedValue(
+    		strategy = GenerationType.SEQUENCE,
+    		generator = "SEQ_USERS_GENERATOR"
+    )
+    @SequenceGenerator( // 필드 레벨에 작성하는 것을 권장
+            name = "SEQ_USERS_GENERATOR",
+            sequenceName = "seq_users",
+            allocationSize = 1
+    )
+    @Column(name = "USER_ID")
     private Long userId;
 
-    @Column(name = "LOGIN_ID", nullable = false, unique = true)
+    @Column(name = "LOGIN_ID")
     private String loginId;
 
     @Column(nullable = false)
@@ -42,7 +58,7 @@ public class UserEntity {
     private String address;
 
     @Column
-    private Integer points = 0;
+    private Integer points; // default 0
 
     @Column(name = "REFRESH_TOKEN")
     private String refreshToken;
@@ -52,7 +68,7 @@ public class UserEntity {
             nullable = false,
             columnDefinition = "CHAR(1)"
     )
-    private String adminFlag = "N";
+    private String adminFlag; // default 'N'
 
     @Column(name = "DELETED_AT")
     private Timestamp deletedAt;
